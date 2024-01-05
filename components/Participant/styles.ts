76a51d2da8175affb58a3858928927680eb7c241
Julia Faria <juliamfbonifacio@gmail.com>
2024-01-05 10:43:41 -0300

    update styles

--- components/Participant/styles.ts
+++ components/Participant/styles.ts
@@ -3,45 +3,30 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
     container: {
       width: '100%',
-
       flexDirection: 'row',
-
       alignItems: 'center',
-
       marginTop: 5,
       marginRight: 10,
-
       borderRadius: 5,
-
       backgroundColor: '#1F1E25',
     },
-
     name : {
       flex: 1,
-
       marginLeft: 16,
-
       fontSize: 16,
-
       color: 'white',
     },
-
     button: {
       width: 56,
       height: 56,
-  
       justifyContent: 'center',
       alignItems: 'center',
-  
       borderWidth: 1,
       borderRadius: 5,
-      
       backgroundColor: '#CD5C5C',
     },
-  
     buttonText: {
       fontSize: 30,
-  
       color: '#DCDCDC',
     },
 });
